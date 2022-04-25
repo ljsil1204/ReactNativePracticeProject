07bcf62c91495b41b705b0b3e39f2f0482e38296
이진실 <ljsil1204@naver.com>
2022-04-25 21:55:36 +0900

    클래스형 컴포넌트 이번트 처리 체험

--- Components/Hook.js
+++ Components/Hook.js
@@ -6,29 +6,62 @@
  * @flow strict-local
  */
 
- import type {Node} from 'react';
- import React, {Fragment, Component } from 'react';
- import {StyleSheet, View, Text,} from 'react-native';
+  import type {Node} from 'react';
+  import React, {Fragment, Component } from 'react';
+  import {StyleSheet, View, Text, Button,} from 'react-native';
 
- const Hook: () => Node = () => {
-   return (
+
+  // 클래스형 컴포넌트
+  class Hook extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        name : 'lee'
+      }
+    }
+
+    changeName = () => {
+      this.setState({
+        name : 'sil'
+      })
+    }
  
-     <View style = {styles.container}>
-       <Text>Hook 페이지입니다.</Text>
-     </View>
+    render(){
+
+      return(
+        <View style = {styles.container}>
+          <Text>Hook 페이지입니다.</Text>
+          <Text>{this.state.name}님</Text>
+          <Button title='이름수정' onPress={() => this.changeName()}></Button>
+        </View>
+      );
+
+    }
+
+  }
+
+
+  // 함수형 컴포넌트
+  // const Hook: () => Node = () => {
+  //   return (
+  
+  //     <View style = {styles.container}>
+  //       <Text>Hook 페이지입니다.</Text>
+  //     </View>
+  
+  //   );
+  // }
  
-   );
- }
- 
- const styles = StyleSheet.create({
-   container : {
-     flex : 1,
-     alignItems : 'center',
-     justifyContent : 'center'
- 
- 
-   }
- });
- 
- export default Hook;
+  const styles = StyleSheet.create({
+    container : {
+      flex : 1,
+      alignItems : 'center',
+      justifyContent : 'center'
+  
+  
+    }
+  });
+  
+  export default Hook;
  
