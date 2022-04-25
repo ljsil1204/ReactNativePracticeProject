0ce4c98e7de1b9f2093660b350cdf6f937021bf8
이진실 <ljsil1204@naver.com>
2022-04-25 15:31:27 +0900

    App Const > App Class로 변경해보기

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import type {Node} from 'react';
+// import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -19,13 +19,13 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
 class Dog extends Component {
   render() {
@@ -44,47 +44,18 @@ class Dog extends Component {
   }
 }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <View style ={styles.container}>
-      <Text style = {styles.hello}>Hellow Wolrd</Text>
-      <Dog type = 'one' />
-      <Dog type = 'two' />
-    </View>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <View style ={styles.container}>
+        <Text style = {styles.hello}>Hellow Wolrd</Text>
+        <Dog type = 'one' />
+        <Dog type = 'two' />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container : {
