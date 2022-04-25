9acee0ffa18c3783c31424499856f668c0eb92cf
이진실 <ljsil1204@naver.com>
2022-04-25 22:04:09 +0900

    함수형 컴포넌트 useState 체험

--- Components/Hook.js
+++ Components/Hook.js
@@ -6,62 +6,68 @@
  * @flow strict-local
  */
 
-  import type {Node} from 'react';
-  import React, {Fragment, Component } from 'react';
-  import {StyleSheet, View, Text, Button,} from 'react-native';
+import type {Node} from 'react';
+import React, {Fragment, Component, useState } from 'react';
+import {StyleSheet, View, Text, Button,} from 'react-native';
 
 
-  // 클래스형 컴포넌트
-  class Hook extends Component {
+// 클래스형 컴포넌트
+// class Hook extends Component {
 
-    constructor(props){
-      super(props);
-      this.state = {
-        name : 'lee'
-      }
-    }
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       name : 'lee'
+//     }
+//   }
 
-    changeName = () => {
-      this.setState({
-        name : 'sil'
-      })
-    }
- 
-    render(){
+//   changeName = () => {
+//     this.setState({
+//       name : 'sil'
+//     })
+//   }
 
-      return(
-        <View style = {styles.container}>
-          <Text>Hook 페이지입니다.</Text>
-          <Text>{this.state.name}님</Text>
-          <Button title='이름수정' onPress={() => this.changeName()}></Button>
-        </View>
-      );
+//   render(){
 
-    }
+//     return(
+//       <View style = {styles.container}>
+//         <Text>Hook 페이지입니다.</Text>
+//         <Text>{this.state.name}님</Text>
+//         <Button title='이름수정' onPress={() => this.changeName()}></Button>
+//       </View>
+//     );
+
+//   }
+
+// }
+
+
+// 함수형 컴포넌트
+const Hook: () => Node = () => {
+
+  const[name, setName] = useState('jin');
+
+  return (
+
+    <View style = {styles.container}>
+      <Text>Hook 페이지입니다.</Text>
+      <Text>{name}님</Text>
+      <Button title='이름수정' onPress={() => setName('sil')}></Button>
+    </View>
+
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center'
+
 
   }
+});
 
-
-  // 함수형 컴포넌트
-  // const Hook: () => Node = () => {
-  //   return (
-  
-  //     <View style = {styles.container}>
-  //       <Text>Hook 페이지입니다.</Text>
-  //     </View>
-  
-  //   );
-  // }
- 
-  const styles = StyleSheet.create({
-    container : {
-      flex : 1,
-      alignItems : 'center',
-      justifyContent : 'center'
-  
-  
-    }
-  });
-  
-  export default Hook;
+export default Hook;
  
