245ba9ba96eacc90f39de8287a84ce30a16fd8c0
이진실 <ljsil1204@naver.com>
2022-04-25 15:35:29 +0900

    필요없는 코드 삭제, Fragment 추가

--- App.js
+++ App.js
@@ -6,63 +6,23 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
-// import type {Node} from 'react';
+import React, {Fragment, Component } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  Image,
-  useColorScheme,
-  View,
 } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-class Dog extends Component {
-  render() {
-    let dogImg = '';
-    if(this.props.type === 'one') {
-      dogImg = require('./assets/dog.jpg');
-    }else if(this.props.type === 'two') {
-      dogImg = require('./assets/dog2.jpg');
-    }
-
-    return (
-      <View>
-        <Image source={dogImg} style={{width:100, height:100}} />
-      </View>
-    );
-  }
-}
-
 
 class App extends Component {
   render() {
     return (
-      <View style ={styles.container}>
-        <Text style = {styles.hello}>Hellow Wolrd</Text>
-        <Dog type = 'one' />
-        <Dog type = 'two' />
-      </View>
+
+      <Fragment></Fragment>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container : {
-      flex : 1,
-      justifyContent : 'center',
-      alignItems : 'center'
-  }
+
 });
 
 export default App;
