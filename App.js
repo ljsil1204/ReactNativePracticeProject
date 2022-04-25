3f218873a7703c6392134effa618155a6fcd4b6b
이진실 <ljsil1204@naver.com>
2022-04-25 16:55:03 +0900

    flex style 사용해보기

--- App.js
+++ App.js
@@ -18,34 +18,10 @@ class App extends Component {
   render() {
     return (
 
-      <View>
-        <View style = {styles.container}>
-          
-          <Image source={require('./assets/dog.jpg')} style={{width:100, height:100}} />
-
-          <View style = {{flexDirection:'column', marginLeft: 15}}>
-            <View style = {styles.listItem}>
-              <Text style = {styles.title}>활동연대</Text>
-              <Text style = {styles.detail}>2000, 2010</Text>
-            </View>
-
-            <View style = {styles.listItem}>
-              <Text style = {[styles.title, styles.point]}>출생</Text>
-              <Text style = {styles.detail}>1994</Text>
-            </View>
-
-            <View style = {styles.listItem}>
-              <Text style = {styles.title}>품종</Text>
-              <Text style = {styles.detail}>개</Text>
-            </View>
-          </View>
-
-        </View>
-
-        <View style = {{width : 50, height : 50, backgroundColor : 'red'}} ></View>
-        <View style = {{width : 100, height : 100, backgroundColor : 'skyblue'}} ></View>
-        <View style = {{width : 140, height : 140, backgroundColor : 'skyblue'}} ></View>
-
+      <View style = {styles.container}>
+        <View style = {{flex:1, backgroundColor : 'red'}}></View>
+        <View style = {{flex:2, backgroundColor : 'yellow'}}></View>
+        <View style = {{flex:3, backgroundColor : 'blue'}}></View>
       </View>
 
     );
@@ -54,25 +30,7 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   container : {
-    marginTop : 50,
-    marginLeft : 20,
-    flexDirection : 'row'
-  },
-  listItem : {
-    flexDirection : 'row',
-    marginBottom : 5
-  },
-  title : {
-    marginRight : 5,
-    color : '#000000',
-    fontWeight: 'bold',
-    fontSize : 15
-  },
-  detail : {
-
-  },
-  point : {
-    color : 'red'
+    flex : 1
   }
 });
 
