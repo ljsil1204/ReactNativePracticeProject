b033b74352fc6be8ecd0bdc71b7b218af6e51ae7
이진실 <ljsil1204@naver.com>
2022-04-26 17:12:08 +0900

    tab navigation Ionicons 적용해보기

--- App.js
+++ App.js
@@ -18,31 +18,47 @@ import {
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
-return (
-  <View style={styles.container}>
-    <Text>Home!</Text>
-  </View>
-);
+  return (
+    <View style={styles.container}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
 function SettingsScreen() {
-return (
-  <View style={styles.container}>
-    <Text>Settings!</Text>
-  </View>
-);
+  return (
+    <View style={styles.container}>
+      <Text>Settings!</Text>
+    </View>
+  );
 }
 
 const App: () => Node = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="홈" component={HomeScreen} />
-        <Tab.Screen name="서브" component={SettingsScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-outline' : 'home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
