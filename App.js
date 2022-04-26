4be0aa81bac9b791722d4b6a4930266386e849e7
이진실 <ljsil1204@naver.com>
2022-04-26 16:01:06 +0900

    tab navigation 예제 사용해보기

--- App.js
+++ App.js
@@ -6,29 +6,55 @@
  * @flow strict-local
  */
 
- import type {Node} from 'react';
- import React, {Fragment, Component } from 'react';
- import {
-   StyleSheet,
-   View,
-   Text,
-   Button,
-   TextInput,
- } from 'react-native';
- 
- const App: () => Node = () => {
-   return (
-      <View></View>
-   );
- }
- 
- const styles = StyleSheet.create({
-   container : {
-     flex : 1,
-     alignItems : 'center',
-     justifyContent : 'center'
-   }
- });
- 
- export default App;
+import type {Node} from 'react';
+import React, {Fragment, Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+return (
+  <View style={styles.container}>
+    <Text>Home!</Text>
+  </View>
+);
+}
+
+function SettingsScreen() {
+return (
+  <View style={styles.container}>
+    <Text>Settings!</Text>
+  </View>
+);
+}
+
+const App: () => Node = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="홈" component={HomeScreen} />
+        <Tab.Screen name="서브" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center'
+  }
+});
+
+export default App;
  
